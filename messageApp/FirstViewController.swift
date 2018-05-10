4cfb093f92cf1cb833e7555d514996a6576fda22
渡邊雄大＠SYS <watanabe-yudai@dmm.com>
2018-05-10 10:54:44 +0900

    Initial Commit

---
+++ messageApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  messageApp
+//
+//  Created by watanabe-yudai on 2018/05/10.
+//  Copyright © 2018年 watanabe-yudai. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ messageApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  messageApp
+//
+//  Created by watanabe-yudai on 2018/05/10.
+//  Copyright © 2018年 watanabe-yudai. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
